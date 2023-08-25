ab74facc6469a31758f9b4155ce562aaef5b8d9b
Qas1m0m0 <Qasim.17@hotmail.com>
2023-08-25 15:39:53 +0100

    added some state

--- index.js
+++ index.js
@@ -3,4 +3,12 @@ let secondCard = 3;
 
 let sumOfCards = firstCard + secondCard;
 
-console.log(sumOfCards);
+
+if (sumOfCards < 21) {
+    console.log('Do you want to draw a new card? 🃏')
+} else if (sumOfCards === 21) {
+    console.log(`You've got Blackjack! 🂫`)
+} else {
+    console.log(`You've lost the game. Unlucky 👎🏼`)
+}
+ console.log(sumOfCards);
