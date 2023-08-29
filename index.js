bbdeb62462f806cc99a0673d58a88c58004780b2
Qas1m0m0 <Qasim.17@hotmail.com>
2023-08-29 14:00:07 +0100

    added newcard function and refactoring code

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 let firstCard = 6;
 let secondCard = 3;
+let cards = [firstCard, secondCard];
 let sumOfCards = firstCard + secondCard;
 let hasBlackJack = false;
 let isAlive = true
 let message = ' '
-let messageEl = document.getElementById('message-el');
+let messageEl = document.querySelector('#message-el');
 let sumEl = document.querySelector('#sum-el');
+let cardsEl = document.querySelector('#cards-el');
 
-function startGame() {  
+function startGame() {
+    renderGame();
+}
+
+function renderGame() {  
+    cardsEl.textContent = 'Cards: ' + cards[0] + ' ' + cards[1];
     sumEl.textContent = 'Sum: ' + sumOfCards;
     if (sumOfCards < 21) {
         message = 'Do you want to draw a new card? 🃏'
@@ -20,4 +27,11 @@ function startGame() {
     }
     messageEl.textContent = message;
    
+}
+
+function newCard() {
+    console.log('Drawing a new card from the deck!');
+    let nextCard = 12
+    sumOfCards +=nextCard
+    renderGame()
 }
